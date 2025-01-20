8675141afa64c10fc6a3f46001c2dd832a1903af
izabelly melo <izabelly.dmelo@mercadolivre.com>
2025-01-20 19:00:44 -0300

    feat: izabelly implement handler functions and add unit tests

--- 17jan/ex03/testing-exercise/internal/handler/hunter.go
+++ 17jan/ex03/testing-exercise/internal/handler/hunter.go
@@ -46,15 +46,15 @@ func (h *Hunter) ConfigurePrey(w http.ResponseWriter, r *http.Request) {
 	log.Println("call ConfigurePrey")
 
 	// request
-	var hunterConfig RequestBodyConfigPrey
-	err := json.NewDecoder(r.Body).Decode(&hunterConfig)
+	var preyConfig RequestBodyConfigPrey
+	err := json.NewDecoder(r.Body).Decode(&preyConfig)
 	if err != nil {
 		response.Error(w, http.StatusBadRequest, "Erro ao decodificar JSON: "+err.Error())
 		return
 	}
 
 	// process
-	h.ht.Configure(hunterConfig.Speed, hunterConfig.Position)
+	h.pr.Configure(preyConfig.Speed, preyConfig.Position)
 
 	// response
 	response.Text(w, http.StatusOK, "A presa está configurada corretamente")
@@ -69,21 +69,41 @@ type RequestBodyConfigHunter struct {
 // ConfigureHunter configures the hunter.
 func (h *Hunter) ConfigureHunter() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// request
+		var hunterConfig RequestBodyConfigHunter
 
-		// process
+		err := json.NewDecoder(r.Body).Decode(&hunterConfig)
+		if err != nil {
+			response.Error(w, http.StatusBadRequest, "invalid request body")
+			return
+		}
 
-		// response
+		h.ht.Configure(hunterConfig.Speed, hunterConfig.Position)
+		response.JSON(w, http.StatusOK, map[string]string{"message": "hunter config successfully"})
 	}
 }
 
-// Hunt hunts the prey.
+// Hunt handles the hunting process.
 func (h *Hunter) Hunt() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// request
+		duration, err := h.ht.Hunt(h.pr)
+		captured := true
+		if err != nil {
+			if err.Error() == "prey not captured" {
+				captured = false
+			} else {
+				response.JSON(w, http.StatusInternalServerError, map[string]string{
+					"status":  "Error",
+					"message": "internal server error",
+				})
+				return
+			}
+		}
 
-		// process
-
-		// response
+		response.JSON(w, http.StatusOK, map[string]any{
+			"status":       "Success",
+			"message":      "prey hunted successfully",
+			"preyCaptured": captured,
+			"duration":     duration,
+		})
 	}
 }
--- 17jan/ex03/testing-exercise/internal/handler/hunter_test.go
+++ 17jan/ex03/testing-exercise/internal/handler/hunter_test.go
@@ -3,8 +3,10 @@ package handler
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testdoubles/internal/hunter"
 	"testdoubles/internal/positioner"
 	"testdoubles/internal/prey"
@@ -12,6 +14,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHunter_ConfigurePrey(t *testing.T) {
@@ -51,4 +54,169 @@ func TestHunter_ConfigurePrey(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, recorder.Code)
 	assert.Equal(t, "A presa está configurada corretamente", recorder.Body.String())
+}
+
+// Configuração incorreta do caçador, problemas com o corpo da solicitação. Retorna um 400
+func TestHunter_IncorrectConfigure(t *testing.T) {
+
+	t.Run("Configuração incorreta do caçador", func(t *testing.T) {
+		ws := hunter.WhiteShark{}
+		pt := prey.Tuna{}
+
+		h := NewHunter(&ws, &pt)
+		configureHunter := h.ConfigureHunter()
+		statusCode := http.StatusBadRequest
+		body := "invalid request body"
+
+		req, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(body))
+		require.NoError(t, err)
+
+		res := httptest.NewRecorder()
+
+		configureHunter(res, req)
+		expectedBody := map[string]interface{}{"status": "Bad Request", "message": "invalid request body"}
+
+		var response map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&response)
+		assert.NoError(t, err)
+
+		assert.Equal(t, statusCode, res.Code)
+		assert.Equal(t, expectedBody, response)
+	})
+}
+
+// O tubarão consegue capturar a presa, exibe uma mensagem de "caça concluída" e dados com informações sobre se ela foi capturada, além do intervalo de tempo. Retorna um 200
+func TestHunter_HunterSuccess(t *testing.T) {
+
+	t.Run("O tubarão consegue capturar a presa", func(t *testing.T) {
+		ws := RequestBodyConfigHunter{
+			Speed: 10.5,
+			Position: &positioner.Position{
+				X: 100,
+				Y: 200,
+			},
+		}
+
+		pt := prey.NewTuna(0.4, &positioner.Position{X: 0.0, Y: 0.0, Z: 0.0})
+
+		tutuba := hunter.NewHunterMock()
+		tutuba.HuntFunc = func(pr prey.Prey) (duration float64, err error) {
+			return 5.0, nil
+		}
+
+		h := NewHunter(tutuba, pt)
+		body, err := json.Marshal(map[string]interface{}{"hunter": ws, "prey": pt})
+		require.NoError(t, err)
+
+		req, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(string(body)))
+		require.NoError(t, err)
+
+		res := httptest.NewRecorder()
+
+		h.Hunt()(res, req)
+
+		expectedBody := map[string]interface{}{
+			"status":       "Success",
+			"message":      "prey hunted successfully",
+			"preyCaptured": true,
+			"duration":     5.0,
+		}
+
+		var response map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&response)
+		assert.NoError(t, err)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+		assert.Equal(t, expectedBody, response)
+	})
+}
+
+// O tubarão não consegue caçar a presa, exibe uma mensagem de "caçada concluída", exibindo também os mesmos dados acima. Retorna um 200
+func TestHunter_HunterNotSuccess(t *testing.T) {
+	t.Run("O tubarão consegue capturar a presa", func(t *testing.T) {
+		ws := RequestBodyConfigHunter{
+			Speed: 1.5,
+			Position: &positioner.Position{
+				X: 100,
+				Y: 200,
+			},
+		}
+
+		pt := prey.NewTuna(11.5, &positioner.Position{X: 100.0, Y: 200.0, Z: 0.0})
+
+		tutuba := hunter.NewHunterMock()
+		tutuba.HuntFunc = func(pr prey.Prey) (duration float64, err error) {
+			return 5.0, errors.New("prey not captured")
+		}
+
+		h := NewHunter(tutuba, pt)
+		body, err := json.Marshal(map[string]interface{}{"hunter": ws, "prey": pt})
+		require.NoError(t, err)
+
+		req, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(string(body)))
+		require.NoError(t, err)
+
+		res := httptest.NewRecorder()
+
+		h.Hunt()(res, req)
+
+		expectedBody := map[string]interface{}{
+			"status":       "Success",
+			"message":      "prey hunted successfully",
+			"preyCaptured": false,
+			"duration":     5.0,
+		}
+
+		var response map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&response)
+		assert.NoError(t, err)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+		assert.Equal(t, expectedBody, response)
+	})
+}
+
+// Caso haja um erro diferente daquele que o caçador não conseguiu caçar, bem como um estagiário, retorne um 500
+func TestHunter_HunterError(t *testing.T) {
+
+	t.Run("o caçador não conseguiu caçar", func(t *testing.T) {
+		ws := RequestBodyConfigHunter{
+			Speed: 11.5,
+			Position: &positioner.Position{
+				X: 100,
+				Y: 200,
+			},
+		}
+
+		pt := prey.NewTuna(11.5, &positioner.Position{X: 100.0, Y: 200.0, Z: 0.0})
+
+		tutuba := hunter.NewHunterMock()
+		tutuba.HuntFunc = func(pr prey.Prey) (duration float64, err error) {
+			return 0, errors.New("error")
+		}
+
+		h := NewHunter(tutuba, pt)
+		body, err := json.Marshal(map[string]interface{}{"hunter": ws, "prey": pt})
+		require.NoError(t, err)
+
+		req, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(string(body)))
+		require.NoError(t, err)
+
+		res := httptest.NewRecorder()
+
+		h.Hunt()(res, req)
+
+		expectedBody := map[string]interface{}{
+			"status":  "Error",
+			"message": "internal server error",
+		}
+
+		var response map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&response)
+		assert.NoError(t, err)
+
+		assert.Equal(t, http.StatusInternalServerError, res.Code)
+		assert.Equal(t, expectedBody, response)
+	})
+
 }
