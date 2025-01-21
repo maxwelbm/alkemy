576daa726598ce05c1ec68a114725451bc476f79
Agata Domingues <124289385+htadmg@users.noreply.github.com>
2025-01-21 10:49:16 -0300

    Update hunter_test.go

--- 17jan/ex03/testing-exercise/internal/handler/hunter_test.go
+++ 17jan/ex03/testing-exercise/internal/handler/hunter_test.go
@@ -1,54 +1,214 @@
 package handler
 
 import (
-	"bytes"
-	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testdoubles/internal/hunter"
-	"testdoubles/internal/positioner"
 	"testdoubles/internal/prey"
-	"testdoubles/internal/simulator"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestHunter_ConfigurePrey(t *testing.T) {
-	requestBody := RequestBodyConfigPrey{
-		Speed: 10.5,
-		Position: &positioner.Position{
-			X: 100,
-			Y: 200,
-		},
-	}
-	body, _ := json.Marshal(requestBody)
+func TestHandlerConfigurePrey(t *testing.T) {
+	t.Run("case 1: prey configured successfully", func(t *testing.T) {
+		// arrange
+		pr := prey.NewPreyStub()
+		hd := NewHunter(nil, pr)
+		hdFunc := hd.ConfigurePrey()
 
-	req, err := http.NewRequest(http.MethodGet, "/hunter/configure-prey", bytes.NewReader(body))
-	if err != nil {
-		t.Fatalf("Não foi possível criar a requisição: %v", err)
-	}
+		request := httptest.NewRequest("POST", "/", strings.NewReader(
+			`{"speed": 5.9, "position": {"X": 0.0, "Y": 0.0, "Z": 0.0}}`,
+		))
+		request.Header.Set("Content-Type", "application/json")
+		response := httptest.NewRecorder()
 
-	recorder := httptest.NewRecorder()
+		// act
+		hdFunc(response, request)
 
-	ps := positioner.NewPositionerDefault()
+		// assert
+		expectedCode := http.StatusOK
+		expectedBody := `{"message":"A presa está configurada corretamente","data":null}`
+		expectedHeaders := http.Header{"Content-Type": []string{"application/json"}}
 
-	sm := simulator.NewCatchSimulatorDefault(&simulator.ConfigCatchSimulatorDefault{
-		Positioner: ps,
+		require.Equal(t, expectedCode, response.Code)
+		require.JSONEq(t, expectedBody, response.Body.String())
+		require.Equal(t, expectedHeaders, response.Header())
 	})
 
-	ht := hunter.NewWhiteShark(hunter.ConfigWhiteShark{
-		Speed:     3.0,
-		Position:  &positioner.Position{X: 0.0, Y: 0.0, Z: 0.0},
-		Simulator: sm,
+	t.Run("case 2: prey configuration failed - invalid request body", func(t *testing.T) {
+		// arrange
+		hd := NewHunter(nil, nil)
+		hdFunc := hd.ConfigurePrey()
+
+		// JSON malformado (falta uma chave ou valor)
+		request := httptest.NewRequest("POST", "/", strings.NewReader(
+			`{"speed": "invalid", "position": {"X": 0.0, "Y": 0.0}}`, // Aqui o valor de speed é uma string
+		))
+		request.Header.Set("Content-Type", "application/json")
+
+		response := httptest.NewRecorder()
+
+		// act
+		hdFunc(response, request)
+
+		expectedCode := http.StatusBadRequest
+		expectedBody := fmt.Sprintf(
+			`{"status":"%s","message":"%s"}`,
+			http.StatusText(expectedCode),
+			"Erro ao decodificar JSON: json: cannot unmarshal string into Go struct field RequestBodyConfigPrey.speed of type float64",
+		)
+
+		expectedHeaders := http.Header{"Content-Type": []string{"application/json"}}
+
+		// assert
+		require.Equal(t, expectedCode, response.Code)
+		require.JSONEq(t, expectedBody, response.Body.String())
+		require.Equal(t, expectedHeaders, response.Header())
+	})
+}
+
+func TestHandlerConfigureHunter(t *testing.T) {
+	t.Run("case 1: hunter configured successfully", func(t *testing.T) {
+		// arrange
+		// - hunter: mock
+		ht := hunter.NewHunterMock()
+		// - handler
+		hd := NewHunter(ht, nil)
+		hdFunc := hd.ConfigureHunter()
+
+		// act
+		request := httptest.NewRequest("POST", "/", strings.NewReader(
+			`{"speed": 10.0, "position": {"X": 0.0, "Y": 0.0, "Z": 0.0}}`,
+		))
+		request.Header.Set("Content-Type", "application/json")
+		response := httptest.NewRecorder()
+		hdFunc(response, request)
+
+		// assert
+		expectedCode := http.StatusOK
+		expectedBody := `{"message":"hunter configured","data":null}`
+		expectedHeaders := http.Header{"Content-Type": []string{"application/json"}}
+		expectedCallConfigure := 1
+		require.Equal(t, expectedCode, response.Code)
+		require.JSONEq(t, expectedBody, response.Body.String())
+		require.Equal(t, expectedHeaders, response.Header())
+		require.Equal(t, expectedCallConfigure, ht.Calls.Configure)
 	})
 
-	pr := prey.NewTuna(0.4, &positioner.Position{X: 0.0, Y: 0.0, Z: 0.0})
+	t.Run("case 2: hunter configured failed  - invalid request body", func(t *testing.T) {
+		// arrange
+		// - handler
+		hd := NewHunter(nil, nil)
+		hdFunc := hd.ConfigureHunter()
 
-	h := NewHunter(ht, pr)
+		// act
+		request := httptest.NewRequest("POST", "/", strings.NewReader(
+			`invalid request body`,
+		))
+		request.Header.Set("Content-Type", "application/json")
+		response := httptest.NewRecorder()
+		hdFunc(response, request)
 
-	h.ConfigurePrey(recorder, req)
+		// assert
+		expectedCode := http.StatusBadRequest
+		expectedBody := fmt.Sprintf(
+			`{"status":"%s","message":"%s"}`,
+			http.StatusText(expectedCode),
+			"Erro ao decodificar JSON: invalid character 'i' looking for beginning of value",
+		)
+		expectedHeaders := http.Header{"Content-Type": []string{"application/json"}}
+		require.Equal(t, expectedCode, response.Code)
+		require.JSONEq(t, expectedBody, response.Body.String())
+		require.Equal(t, expectedHeaders, response.Header())
+	})
+}
 
-	assert.Equal(t, http.StatusOK, recorder.Code)
-	assert.Equal(t, "A presa está configurada corretamente", recorder.Body.String())
+func TestHandlerHunt(t *testing.T) {
+	t.Run("case 1: success to hunt", func(t *testing.T) {
+		// arrange
+		// - hunter: mock
+		ht := hunter.NewHunterMock()
+		ht.HuntFunc = func(pr prey.Prey) (duration float64, err error) {
+			return 100.0, nil
+		}
+		// - handler
+		hd := NewHunter(ht, nil)
+		hdFunc := hd.Hunt()
+
+		// act
+		request := httptest.NewRequest("POST", "/", nil)
+		response := httptest.NewRecorder()
+		hdFunc(response, request)
+
+		// assert
+		expectedCode := http.StatusOK
+		expectedBody := `{"message":"hunt done","data":{"success":true,"duration":100.0}}`
+		expectedHeaders := http.Header{"Content-Type": []string{"application/json"}}
+		expectedCallHunt := 1
+		require.Equal(t, expectedCode, response.Code)
+		require.JSONEq(t, expectedBody, response.Body.String())
+		require.Equal(t, expectedHeaders, response.Header())
+		require.Equal(t, expectedCallHunt, ht.Calls.Hunt)
+	})
+
+	t.Run("case 2: fail to hunt - fail hunting the prey", func(t *testing.T) {
+		// arrange
+		// - hunter: mock
+		ht := hunter.NewHunterMock()
+		ht.HuntFunc = func(pr prey.Prey) (duration float64, err error) {
+			return 0.0, hunter.ErrCanNotHunt
+		}
+		// - handler
+		hd := NewHunter(ht, nil)
+		hdFunc := hd.Hunt()
+
+		// act
+		request := httptest.NewRequest("POST", "/", nil)
+		response := httptest.NewRecorder()
+		hdFunc(response, request)
+
+		// assert
+		expectedCode := http.StatusOK
+		expectedBody := `{"message":"hunt done","data":{"success":false,"duration":0.0}}`
+		expectedHeaders := http.Header{"Content-Type": []string{"application/json"}}
+		expectedCallHunt := 1
+		require.Equal(t, expectedCode, response.Code)
+		require.JSONEq(t, expectedBody, response.Body.String())
+		require.Equal(t, expectedHeaders, response.Header())
+		require.Equal(t, expectedCallHunt, ht.Calls.Hunt)
+	})
+
+	t.Run("case 3: fail to hunt - internal server error", func(t *testing.T) {
+		// arrange
+		// - hunter: mock
+		ht := hunter.NewHunterMock()
+		ht.HuntFunc = func(pr prey.Prey) (duration float64, err error) {
+			return 0.0, errors.New("internal server error")
+		}
+		// - handler
+		hd := NewHunter(ht, nil)
+
+		// act
+		request := httptest.NewRequest("POST", "/", nil)
+		response := httptest.NewRecorder()
+		hd.Hunt()(response, request)
+
+		// assert
+		expectedCode := http.StatusInternalServerError
+		expectedBody := fmt.Sprintf(
+			`{"status":"%s","message":"%s"}`,
+			http.StatusText(expectedCode),
+			"internal server error",
+		)
+		expectedHeaders := http.Header{"Content-Type": []string{"application/json"}}
+		expectedCallHunt := 1
+		require.Equal(t, expectedCode, response.Code)
+		require.JSONEq(t, expectedBody, response.Body.String())
+		require.Equal(t, expectedHeaders, response.Header())
+		require.Equal(t, expectedCallHunt, ht.Calls.Hunt)
+	})
 }
